457efd9b85e3ba13ff438cebaae16a8d45181363
takanori-hozumi <takadddan@gmail.com>
2021-12-11 21:04:14 +0900

    画像投稿機能(railsのバージョンが6.0以上になるとrifileの画像投稿が使えない)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9acbe7272f9b7ea84d43a46dc3bd693d3b404e56c2aeb2c252f91742f476518b2f233aed3e5b35d5d0d7fe8e50122edf9e4a44b065da772d825a213c3d019296'
