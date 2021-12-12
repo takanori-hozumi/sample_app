22f40c3ebbb42cdb61abedc7004af12dc37771e0
takanori-hozumi <takadddan@gmail.com>
2021-12-12 14:49:02 +0900

    レイアウト等を整えた

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,11 +6,14 @@ class TodolistsController < ApplicationController
 
   def create
     # １. データを新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # ３. 一覧画面へリダイレクト
-    redirect_to todolist_path(list.id)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+
   end
 
   def index
@@ -29,6 +32,12 @@ class TodolistsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
 
 
